4cdc875df726b5407ba4f0ede3af58e74ef6e0ea
EpicMo <1982742309@qq.com>
2023-10-27 15:10:44 +0800

    feat: use fixed port

--- run_api.sh
+++ run_api.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Pre Api settings script by @EpicMo
-port=8999 # The port of Lora-Scripts Server
-
-python -m accelerate.commands.launch ${launchArgs[@]} --num_cpu_threads_per_process=8 "./sd-scripts/train_api.py" $port
+python -m accelerate.commands.launch ${launchArgs[@]} --num_cpu_threads_per_process=8 "./sd-scripts/train_api.py"
